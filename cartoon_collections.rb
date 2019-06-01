92e1ae4893ce7f083027f91db99634e2eac4542a
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-01 13:40:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,9 @@ end
 
 def long_planeteer_calls(array)
   array.each do |x|
-        if x.size == 4
+        if x.length == 4
           return true
-        elsif x.size > 4
+        elsif x.length > 4
           return true
         else 
           return false
